f8f9534ea7c2845ca50b5523070e7a6f8e501eb4
Joseph Larmarange <joseph@larmarange.net>
2018-07-16 22:14:41 +0200

    première version

---
+++ lang/paquet-spipr_joseph_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// 
+///  Fichier produit par PlugOnet
+// Module: paquet-spipr_blog
+// Langue: fr
+// Date: 29-11-2012 16:42:19
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// S
+	'spipr_joseph_description' => 'Adapté de SPIPr-blog, avec des inspirations des thèmes Original et Balita de colorlib.com',
+	'spipr_joseph_slogan' => 'Squelettes de Blog pour joseph.larmarange.net',
+);
+?>
---
+++ spipr_joseph_options.php
@@ -0,0 +1,14 @@
+<?php
+if (!isset($GLOBALS['z_blocs']))
+	$GLOBALS['z_blocs'] = array('content','aside','head','head_js','header','footer');
+
+$GLOBALS['debut_intertitre'] = "\n<h2 class=\"spip\">\n";
+$GLOBALS['fin_intertitre'] = "</h2>\n";
+
+define('_MENUS_EXTRA_CLASS', 'dropdown');
+
+define('_LOGO_MAX_HEIGHT', 0);
+define('_LOGO_MAX_WIDTH', 0);
+define('_LOGO_MAX_SIZE', 0);
+
+?>
